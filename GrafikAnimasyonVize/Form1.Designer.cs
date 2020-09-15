1195b670fdb2ec3ad1322b53bbea4b9926d401dc
Yunus İNAN <49419869+ynsinan@users.noreply.github.com>
2020-09-15 14:37:10 +0300

    Flag

---
+++ GrafikAnimasyonVize/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace GrafikAnimasyonVize
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ÇİZ = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Cizim = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Cizim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ÇİZ
+            // 
+            this.ÇİZ.Location = new System.Drawing.Point(469, 150);
+            this.ÇİZ.Name = "ÇİZ";
+            this.ÇİZ.Size = new System.Drawing.Size(96, 33);
+            this.ÇİZ.TabIndex = 0;
+            this.ÇİZ.Text = "ÇİZ";
+            this.ÇİZ.UseVisualStyleBackColor = true;
+            this.ÇİZ.Click += new System.EventHandler(this.ÇİZ_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(469, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "TEMİZLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(469, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "KAPAT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Cizim
+            // 
+            this.Cizim.Location = new System.Drawing.Point(12, 12);
+            this.Cizim.Name = "Cizim";
+            this.Cizim.Size = new System.Drawing.Size(451, 255);
+            this.Cizim.TabIndex = 3;
+            this.Cizim.TabStop = false;
+            this.Cizim.Paint += new System.Windows.Forms.PaintEventHandler(this.Cizim_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 279);
+            this.Controls.Add(this.Cizim);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ÇİZ);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Cizim)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ÇİZ;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        public System.Windows.Forms.PictureBox Cizim;
+    }
+}
+
---
+++ GrafikAnimasyonVize/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrafikAnimasyonVize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //bayrak çerçevesi noktaları
+        private PointF[] NoktalarCerceve1 = new PointF[4];
+        private PointF[] NoktalarCerceve2 = new PointF[4];
+        private PointF[] NoktalarCerceve3 = new PointF[4];
+        private PointF[] NoktalarCerceve4 = new PointF[4];
+        private PointF[] NoktalarCerceve5 = new PointF[4];
+        private PointF[] NoktalarCerceve6 = new PointF[4];
+
+        //yıldız noktaları
+        private PointF[] NoktalarYildiz1 = new PointF[4];
+        private PointF[] NoktalarYildiz2 = new PointF[4];
+        private PointF[] NoktalarYildiz3 = new PointF[4];
+        private PointF[] NoktalarYildiz4 = new PointF[4];
+        private PointF[] NoktalarYildiz5 = new PointF[4];
+        private PointF[] NoktalarYildiz6 = new PointF[4];
+        private PointF[] NoktalarYildiz7 = new PointF[4];
+        private PointF[] NoktalarYildiz8 = new PointF[4];
+        private PointF[] NoktalarYildiz9 = new PointF[4];
+        private PointF[] NoktalarYildiz10 = new PointF[4];
+
+        //hilal noktaları
+        private PointF[] NoktalarHilal1 = new PointF[4];
+        private PointF[] NoktalarHilal2 = new PointF[4];
+
+
+
+
+
+
+
+
+        public static void Temizle(PointF[] noktalar)
+        {
+           noktalar[0].X = 0;
+           noktalar[0].Y = 0;
+           noktalar[1].X = 0;
+           noktalar[1].Y = 0;
+           noktalar[2].X = 0;
+           noktalar[2].Y = 0;
+           noktalar[3].X = 0;
+           noktalar[3].Y = 0;
+        }
+        
+        public static void bCiz(PointF[] noktalar,float n1,float n2,float n3,float n4,float n5,float n6,float n7,float n8)
+        {
+
+            noktalar[0].X =n1 ;
+            noktalar[0].Y =n2 ;
+            noktalar[3].X =n3 ;
+            noktalar[3].Y =n4 ;
+            noktalar[1].X =n5 ;
+            noktalar[1].Y =n6 ;
+            noktalar[2].X =n7 ;
+            noktalar[2].Y =n8 ;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Bayrak çerçevesii
+            Temizle(NoktalarCerceve1);
+            Temizle(NoktalarCerceve2);
+            Temizle(NoktalarCerceve3);
+            Temizle(NoktalarCerceve4);
+            Temizle(NoktalarCerceve5);
+            Temizle(NoktalarCerceve6);
+
+            Temizle(NoktalarYildiz1);
+            Temizle(NoktalarYildiz2);
+            Temizle(NoktalarYildiz3);
+            Temizle(NoktalarYildiz4);
+            Temizle(NoktalarYildiz5);
+            Temizle(NoktalarYildiz6);
+            Temizle(NoktalarYildiz7);
+            Temizle(NoktalarYildiz8);
+            Temizle(NoktalarYildiz9);
+            Temizle(NoktalarYildiz10);
+
+            Temizle(NoktalarHilal1);
+            Temizle(NoktalarHilal2);
+
+            Cizim.Invalidate();
+        }
+        public static void yCiz(PointF[] nokta, float n1, float n2, float n3, float n4)
+        {
+            bCiz(nokta, n1, n2, n3, n4, n1, n2, n3, n4);
+        }
+        private void ÇİZ_Click(object sender, EventArgs e)
+        {
+            
+
+            //sol yan çerçeve
+            bCiz(NoktalarCerceve1, 40, 40, 40, 180, 40, 60, 40, 60);
+
+            //sol üst çerçeve
+            bCiz(NoktalarCerceve2, 40, 40, 180, 40, 50, 60, 170, 60);
+
+            //sağ üst çerçeve
+            bCiz(NoktalarCerceve3, 320, 40, 180, 40, 300, 20, 200, 20);
+
+            //sağ yan çerçeve
+            bCiz(NoktalarCerceve4, 320, 40, 320, 180, 320, 40, 320, 180);
+
+            //sol alt çerçeve
+            bCiz(NoktalarCerceve5, 40, 180, 180, 180, 60, 200, 160, 200);
+
+            //sağ alt çerçeve 
+            bCiz(NoktalarCerceve6, 320, 180, 180, 180, 300, 160, 200, 160);
+
+            //y1
+            yCiz(NoktalarYildiz1, 220, 60, 200, 90);
+
+            //y2
+            yCiz(NoktalarYildiz2, 220, 60, 240, 90);
+
+            //y3
+            yCiz(NoktalarYildiz3, 280, 90, 240, 90);
+
+            //y4
+            yCiz(NoktalarYildiz4, 280, 90, 250, 120);
+
+            //y5
+            yCiz(NoktalarYildiz5, 270, 150, 250, 120);
+
+            //y6
+            yCiz(NoktalarYildiz6, 270, 150, 220, 130);
+
+            //y7
+            yCiz(NoktalarYildiz7, 170, 150, 220, 130);
+
+            //y8
+            yCiz(NoktalarYildiz8, 170, 150, 190, 120);
+
+            //y9
+            yCiz(NoktalarYildiz9, 160, 90, 190, 120);
+
+            //10
+            yCiz(NoktalarYildiz10, 160, 90, 200, 90);
+
+            //hilal1
+            bCiz(NoktalarHilal1, 120, 70, 120, 150, 50, 50, 50, 170);
+
+            //hilal
+            bCiz(NoktalarHilal2, 120, 70, 120, 150, 70, 70, 70, 150);
+
+
+
+            Cizim.Invalidate();
+
+        }
+
+        private void Cizim_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Cizim.BackColor);
+
+
+            //hilal
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                NoktalarHilal1[0], NoktalarHilal1[1], NoktalarHilal1[2], NoktalarHilal1[3]);
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                NoktalarHilal2[0], NoktalarHilal2[1], NoktalarHilal2[2], NoktalarHilal2[3]);
+
+
+
+            //çerçeve
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                NoktalarCerceve1[0], NoktalarCerceve1[1], NoktalarCerceve1[2], NoktalarCerceve1[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                NoktalarCerceve2[0], NoktalarCerceve2[1], NoktalarCerceve2[2], NoktalarCerceve2[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                NoktalarCerceve3[0], NoktalarCerceve3[1], NoktalarCerceve3[2], NoktalarCerceve3[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                NoktalarCerceve4[0], NoktalarCerceve4[1], NoktalarCerceve4[2], NoktalarCerceve4[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                NoktalarCerceve5[0], NoktalarCerceve5[1], NoktalarCerceve5[2], NoktalarCerceve5[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                NoktalarCerceve6[0], NoktalarCerceve6[1], NoktalarCerceve6[2], NoktalarCerceve6[3]);
+
+
+
+
+            //yıldız
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                NoktalarYildiz1[0], NoktalarYildiz1[1], NoktalarYildiz1[2], NoktalarYildiz1[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                            NoktalarYildiz2[0], NoktalarYildiz2[1], NoktalarYildiz2[2], NoktalarYildiz2[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                            NoktalarYildiz3[0], NoktalarYildiz3[1], NoktalarYildiz3[2], NoktalarYildiz3[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                            NoktalarYildiz4[0], NoktalarYildiz4[1], NoktalarYildiz4[2], NoktalarYildiz4[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                            NoktalarYildiz5[0], NoktalarYildiz5[1], NoktalarYildiz5[2], NoktalarYildiz5[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                            NoktalarYildiz6[0], NoktalarYildiz6[1], NoktalarYildiz6[2], NoktalarYildiz6[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                            NoktalarYildiz7[0], NoktalarYildiz7[1], NoktalarYildiz7[2], NoktalarYildiz7[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                            NoktalarYildiz8[0], NoktalarYildiz8[1], NoktalarYildiz8[2], NoktalarYildiz8[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                            NoktalarYildiz9[0], NoktalarYildiz9[1], NoktalarYildiz9[2], NoktalarYildiz9[3]);
+
+            Bezier_Egrisi.Bezier_Ciz(e.Graphics, Pens.Red, 0.01f,
+                            NoktalarYildiz10[0], NoktalarYildiz10[1], NoktalarYildiz10[2], NoktalarYildiz10[3]);
+
+        }
+    }
+}
